54075dc3615a3d93962147c2fc238fbec8d9ecbc
AbderrahimFizazi <abderahim.fazazi@etu.uae.ac.ma>
2022-12-15 05:22:56 -0800

    back

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for a in str:
         s = ord(a)
         if s > 96 and s < 123:
-            s = ord(a) -32
+            s = ord(a) - 32
         print("{:c}".format(s), end='')
     print('')
