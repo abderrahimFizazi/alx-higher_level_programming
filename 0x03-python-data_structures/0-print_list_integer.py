4e7fc8744ba2777f55dacc208c2b773aef2c74e1
AbderrahimFizazi <abderahim.fazazi@etu.uae.ac.ma>
2022-12-17 12:46:05 -0800

    back

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
-        return None
-    else:
-        return my_list[idx]
+def print_list_integer(my_list=[]):
+    for a in range(len(my_list)):
+        print(my_list[a])
