ad2c71af04bc5df9df4a64d30538a0b87622395e
AbderrahimFizazi <abderahim.fazazi@etu.uae.ac.ma>
2022-12-19 02:07:00 -0800

    back

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     summ = 0
-    for i in range(0,x):
+    for i in range(x):
         try:
-            summ = summ * 10 + my_list[i]
-        except:
-            print(summ)
+            print(my_list[i], end='')
+            summ += 1
+        except IndexError:
+            print()
             return i
-    print(summ)
+    print()
     return i+1
