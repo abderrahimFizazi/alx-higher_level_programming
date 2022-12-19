d402d9d06df96a656187bd60c4738ab47bee9d21
AbderrahimFizazi <abderahim.fazazi@etu.uae.ac.ma>
2022-12-19 06:32:29 -0800

    back

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0:
+        return my_list
+    else:
+        for a in my_list:
+            if a == my_list[idx]:
+                my_list.remove(a)
+        return my_list
