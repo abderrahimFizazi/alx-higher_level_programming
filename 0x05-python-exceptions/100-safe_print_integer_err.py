d48ae43f2969cda000616a72c8c01bbd8337cf2c
AbderrahimFizazi <abderahim.fazazi@etu.uae.ac.ma>
2022-12-19 03:25:02 -0800

    back

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
-#!/bin/python3
-def safe_print_integer(value):
+#!/usr/bin/python3
+def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError) as er:
+    except (ValueError, TypeError) as error:
         import sys
-        print("Exception: {}".format(er), file=sys.stderr)
+        print("Exception: {}".format(error), file=sys.stderr)
         return False
+    else:
+    return True
