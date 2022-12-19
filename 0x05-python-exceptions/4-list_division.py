d211fbcc680d6ba2bb5b953eb5c26652fc093362
AbderrahimFizazi <abderahim.fazazi@etu.uae.ac.ma>
2022-12-19 03:09:25 -0800

    back

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,7 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             res = 0
         except IndexError:
             print("out of range")
-            break
+            res = 0
         except (ValueError, TypeError):
             print("wrong type")
             res = 0
