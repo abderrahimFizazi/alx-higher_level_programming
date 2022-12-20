ab05b62254ba1cd5cd723d9a8989eae78352127a
AbderrahimFizazi <abderahim.fazazi@etu.uae.ac.ma>
2022-12-20 12:59:18 -0800

    back

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# 5-square.py by abdo
+"""Defines a square """
+
+
+class Square:
+    """Initializing this square class
+        Args: size - represnets the size of the square defined
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves size of square"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        return (self.__size ** 2)
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for a in range(self.size):
+                for b in range(self.size):
+                    print('#',end='')
+                print()
