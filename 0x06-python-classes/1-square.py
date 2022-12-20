b56b691f622be4822bc40f4adba1533f5fcb1652
AbderrahimFizazi <abderahim.fazazi@etu.uae.ac.ma>
2022-12-20 05:12:14 -0800

    back

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-# 0-square.py by Ehoneah Obed
+# 1-square.py by abdo
 """Defines a square """
 
 
 class Square:
+    """Initializing this square class
+        Args: size - represnets the size of the square defined
+    """
 
     def __init__(self, size):
         self.__size = size
